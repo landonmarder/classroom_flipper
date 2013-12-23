ea346225be7c9abaea8fc3accceeaaec9dcf4748
landonmarder <landon.marder@gmail.com>
2013-12-23 18:44:45 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ClassroomFlipper::Application.config.secret_key_base = '558d6d134fb4c2a39f068ff5b8b2125b1dacb0b61aa1cb3417d0c9c53841d57434b070b44bdf8268314939b1a3a4539247b54532f1c6ec8ef1531b265b53243c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ClassroomFlipper::Application.config.session_store :cookie_store, key: '_classroom_flipper_session'
