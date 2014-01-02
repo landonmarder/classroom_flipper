cf9d75bdae1112c0fb352e8c512e0e60bab7c5cc
landonmarder <landon.marder@gmail.com>
2014-01-02 15:56:00 -0500

    happy path green

---
+++ spec/features/user_signs_out_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+feature 'user signs out' do
+  # ACCEPTANCE CRITERIA
+  # * I am a signed in user
+  # * I must click on the sign out page
+  # * I must click sign out
+  # * I must be returned to the home page
+
+  scenario 'an existing user signs out' do
+    user = FactoryGirl.create(:teacher)
+    visit new_user_session_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+
+    click_button 'Sign In'
+
+    expect(page).to have_content("Welcome Back!")
+    expect(page).to have_content("Sign Out")
+    expect(page).to_not have_content("Sign In")
+
+    click_link 'Sign Out'
+
+    expect(page).to_not have_content("Sign Out")
+    expect(page).to have_content("Sign In")
+  end
+
+  scenario 'a user that is not authenticated cannot sign out' do
+  end
+end
