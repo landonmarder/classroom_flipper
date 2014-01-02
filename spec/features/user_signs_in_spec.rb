8921e1503c8ec85ba89b9c785cd43e00d6af1575
landonmarder <landon.marder@gmail.com>
2014-01-02 10:13:07 -0500

    all tests green

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -48,5 +48,18 @@ feature 'user signs in' do
     expect(page).to have_content('Invalid email or password.')
   end
 
-  scenario 'an already authenticated user cannot re-sign in'
+  scenario 'an already authenticated user cannot re-sign in' do
+    user = FactoryGirl.create(:teacher)
+    visit new_user_session_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+
+    click_button 'Sign In'
+    expect(page).to have_content("Welcome Back!")
+    expect(page).to have_content("Sign Out")
+    expect(page).to_not have_content("Sign In")
+
+    visit new_user_session_path
+    expect(page).to have_content("You are already signed in.")
+  end
 end
