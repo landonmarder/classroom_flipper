39f98b8135839669e168f6be43c78bfccbe095c2
landonmarder <landon.marder@gmail.com>
2014-01-06 17:15:06 -0500

    happy path of student enrolling in a classroom and not being able to enroll if already enrolled pass

--- spec/features/student_follows_classroom_spec.rb
+++ spec/features/student_follows_classroom_spec.rb
@@ -9,6 +9,7 @@ feature 'student follows a classroom' do
   # * I am redirected to my manage classroom page where it shows I am enrolled in the class
 
   let (:student) { FactoryGirl.create(:student) }
+
   scenario 'student searches for a classroom that exists and enrolls' do
     teacher = FactoryGirl.create(:teacher)
     FactoryGirl.create(:classroom, name: 'Biology', user_id: teacher.id)
@@ -16,7 +17,7 @@ feature 'student follows a classroom' do
 
     sign_in_as(student)
     click_link "Manage Classrooms"
-    save_and_open_page
+
     expect(page).to have_content('Manage Classrooms')
     expect(page).to have_content('Biology')
     expect(page).to have_content('Math')
@@ -31,9 +32,29 @@ feature 'student follows a classroom' do
     click_button '+'
     expect(page).to have_content('Thank you for enrolling!')
     expect(page).to have_content('Manage Classrooms')
+    expect(page).to have_content('Enrolled')
   end
 
-  scenario 'student searches for a classroom that does not exist'
+  scenario 'student cannot enroll in a class that they are already enrolled in' do
+    teacher = FactoryGirl.create(:teacher)
+    classroom = FactoryGirl.create(:classroom, name: 'Biology', user_id: teacher.id)
+    FactoryGirl.create(:enrollment, classroom_id: classroom.id, user_id: student.id)
+
+    sign_in_as(student)
+    click_link "Manage Classrooms"
+
+    expect(page).to have_content('Manage Classrooms')
+    expect(page).to have_content('Biology')
+    expect(page).to have_content(teacher.professional_name)
+
+    fill_in 'Classroom Name', with: 'Biology'
+    click_button 'Search'
+
+    expect(page).to have_content('Biology')
+    expect(page).to have_content('Enrolled')
+
+    expect(page).to have_content('Manage Classrooms')
+  end
 
   scenario 'teacher does not have access to follow a classroom'
 
