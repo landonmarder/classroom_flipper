5f0cc0b341fe854c32a1d867f1167f638fdf9fd7
landonmarder <landon.marder@gmail.com>
2014-01-15 11:03:41 -0500

    assignment gets saved

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -2,18 +2,29 @@ class SubmissionsController < ApplicationController
   def new
     @assignment = Assignment.find(params[:assignment_id])
     @question_number = 1
-    @enrollment = @assignment.classroom.enrollments.find_by(user: current_user)
     @submission = @assignment.submissions.new
+    # @submission.enrollment = @enrollment
     @answer = @submission.answers.new
   end
 
   def create
-  @submission = Submission.new()
+    @assignment = Assignment.find(params[:assignment_id])
+    @enrollment = @assignment.classroom.enrollments.find_by(user: current_user)
+    @submission = Submission.new
+
+    @submission.enrollment_id = @enrollment.id
+    @submission.assignment_id = @assignment.id
+
+    if @submission.save
+      flash[:notice] = 'Thank you for completing your assignment!!'
+      redirect_to assignments_path
+    else
+    end
   end
 
 
   private
   def submission_params
-    params.require(:submission).permit(:answer_attributes: [:option_id, :submission_id])
+    params.require(:submission).permit(answer_attributes: [:option_id, :submission_id, :question_id])
   end
 end
