f2f0e1b0f2f9cd6efd86b35854379393b062fd10
landonmarder <landon.marder@gmail.com>
2014-01-16 08:31:55 -0500

    set up testing for assignment results

--- spec/models/submission_spec.rb
+++ spec/models/submission_spec.rb
@@ -11,4 +11,17 @@ describe Submission do
   it { should belong_to(:assignment) }
 
   it { should have_many(:answers).dependent(:destroy) }
+
+  it 'gives the results of a submission for a student' do
+    submission = FactoryGirl.create(:submission)
+    question_one = FactoryGirl.create(:question)
+    option_correct = FactoryGirl.create(:option, question: question_one, weight: 1)
+    answer_one = FactoryGirl.create(:answer, option: option_correct, question: question_one, submission: submission)
+
+    question_two = FactoryGirl.create(:question)
+    option_incorrect = FactoryGirl.create(:option, question: question_two, weight: 0)
+    answer_two = FactoryGirl.create(:answer, option: option_incorrect, question: question_two, submission: submission)
+    binding.pry
+    expect()
+  end
 end
